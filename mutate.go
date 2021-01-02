c3447684cf6e359c7ba2d0e3e633ac2d7e3d5f7e
William Hearn <sylus1984@gmail.com>
2021-01-02 14:39:54 -0500

    feat(minio): Add MinIO mounts to Workflow Runs

--- mutate.go
+++ mutate.go
@@ -157,7 +157,27 @@ func (s *server) mutate(request v1beta1.AdmissionRequest) (v1beta1.AdmissionResp
 	profile := cleanName(pod.Namespace)
 
 	// If we have a notebook, then lets run the logic
-	if _, ok := (pod.ObjectMeta.Labels["notebook-name"]) || (pod.ObjectMeta.Labels["workflows.argoproj.io/workflow"]); ok {
+	if _, ok := pod.ObjectMeta.Labels["notebook-name"]; ok {
+		patches = append(patches, s.addBoathouseInstance("minimal-minio-tenant1", "minio_minimal_tenant1", "https://minimal-tenant1-minio.covid.cloud.statcan.ca", defaultRegion, profile, "/home/jovyan/minio/minimal-tenant1")...)
+		patches = append(patches, s.addBoathouseInstance("premium-minio-tenant1", "minio_premium_tenant1", "https://premium-tenant1-minio.covid.cloud.statcan.ca", defaultRegion, profile, "/home/jovyan/minio/premium-tenant1")...)
+		patches = append(patches, s.addBoathouseInstance("premium-minio-tenant-1", "minio_premium_tenant_1", "https://minio-premium-tenant-1.covid.cloud.statcan.ca", defaultRegion, profile, "/home/jovyan/minio/premium-tenant-1")...)
+		patches = append(patches, s.addBoathouseInstance("standard-minio-tenant-1", "minio_standard_tenant_1", "https://minio-standard-tenant-1.covid.cloud.statcan.ca", defaultRegion, profile, "/home/jovyan/minio/standard-tenant-1")...)
+
+		response.AuditAnnotations = map[string]string{
+			"goofys-injector": "Added MinIO volume mounts",
+		}
+		response.Patch, err = json.Marshal(patches)
+		if err != nil {
+			return response, err
+		}
+
+		response.Result = &metav1.Status{
+			Status: metav1.StatusSuccess,
+		}
+	}
+
+	// If we have a Argo workflow, then lets run the logic
+	if _, ok := pod.ObjectMeta.Labels["workflows.argoproj.io/workflow"]; ok {
 		patches = append(patches, s.addBoathouseInstance("minimal-minio-tenant1", "minio_minimal_tenant1", "https://minimal-tenant1-minio.covid.cloud.statcan.ca", defaultRegion, profile, "/home/jovyan/minio/minimal-tenant1")...)
 		patches = append(patches, s.addBoathouseInstance("premium-minio-tenant1", "minio_premium_tenant1", "https://premium-tenant1-minio.covid.cloud.statcan.ca", defaultRegion, profile, "/home/jovyan/minio/premium-tenant1")...)
 		patches = append(patches, s.addBoathouseInstance("premium-minio-tenant-1", "minio_premium_tenant_1", "https://minio-premium-tenant-1.covid.cloud.statcan.ca", defaultRegion, profile, "/home/jovyan/minio/premium-tenant-1")...)
